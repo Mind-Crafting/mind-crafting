986998d8a663a672a67e58fc5a1d83fe1301668e
Sam Formichella <43454748+sformichella@users.noreply.github.com>
2021-01-20 10:15:39 -0800

    Feature/slack bots refactor (#19)
    * refactor echo I think
    
    * fix command string
    
    * fix slackBot message -> text
    
    * make handler static
    
    * pass slack echo test
    
    * make echo test more fun!
    
    * refactor slack play
    
    * SlackService.play returns
    
    * refactor disconnect
    
    * refactor movement
    
    * add play test
    
    * refactor look
    
    * add look case in SlackService
    
    * remove old look
    
    * fix left/right swapped thing
    
    * fix move left/right again
    
    * just kidding, really fixed it this time
    
    * actually fix move for real this time
    
    * delete viewer stuff
    
    * add forgotten return
    
    * add look center

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,10 +5,18 @@ const app = require('../lib/app');
 
 describe('mind-crafting routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    
   });
 
-  it('should pass', async() => {
-    
+  it('should echo a message', async() => {
+    const response = await request(app)
+      .post('/')
+      .send({
+        event:{
+          text: 'echo this is a regularly scheduled echo for your convenience'
+        }
+      });
+
+    expect(response.body).toEqual({ response: 'this is a regularly scheduled echo for your convenience' });
   });
 });
